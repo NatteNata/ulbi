3f58cc3400c45f873cafe5cd55ab7067cf14d034
natte <natte.noshimo@gmail.com>
2024-10-19 02:22:10 +0300

    Server-side-render - dynamic generation, async getServerSideProps in server-side-component with context

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,13 +1,20 @@
 import {useRouter} from "next/router";
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
-export default function User() {
+export default function User({user}) {
 
-    const {query} = useRouter();
-
-    return (
+     return (
         <>
-            <h2>Пользователь с id {query.id}</h2>
-            <strong>Their name is </strong>
+            <h2>Пользователь с id {user.id}</h2>
+            <strong>Their name is {user.name}</strong>
         </>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+    const {id} = context.params;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const user = await response.json()
+    return {props: {user}}
 }
